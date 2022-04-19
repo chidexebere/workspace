e2306488dd0ec93895452e5bed57b4067e4d6420
Chidi <chidex5@gmail.com>
2022-04-19 16:29:58 +0100

    Add firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAYLmYJtkUtq62jmArwu30XNelP05NaYUs',
+  authDomain: 'workspace-api-2810a.firebaseapp.com',
+  projectId: 'workspace-api-2810a',
+  storageBucket: 'workspace-api-2810a.appspot.com',
+  messagingSenderId: '360411721331',
+  appId: '1:360411721331:web:d252c764b07ea57bc20406',
+  measurementId: 'G-Z95DW9V4YP',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { app, db };
