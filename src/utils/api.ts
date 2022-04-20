f67a0926eb4ad07c1de8df12931f2d603a808de1
Chidi <chidex5@gmail.com>
2022-04-20 02:36:15 +0100

    Add API functions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,17 @@
 import { collection, DocumentData, getDocs } from 'firebase/firestore';
 import { db } from '../firebase/firebase.config';
 
-export const getDashItems = async (item: string) => {
-  const querySnapshot = await getDocs(collection(db, item));
+// export const getItems = async (item: string) => {
+//   const querySnapshot = await getDocs(collection(db, item));
+//   const documents: DocumentData[] = [];
+//   querySnapshot.forEach((doc) => {
+//     documents.push({ id: doc.id, ...doc.data() });
+//   });
+//   return documents;
+// };
+
+export const getItems = async () => {
+  const querySnapshot = await getDocs(collection(db, 'boards'));
   const documents: DocumentData[] = [];
   querySnapshot.forEach((doc) => {
     documents.push({ id: doc.id, ...doc.data() });
