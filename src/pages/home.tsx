dcdf07ae1e562513c4e69c355faf88045366bd75
Chidi <chidex5@gmail.com>
2022-10-03 02:59:52 +0100

    Modify home page button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -20,7 +20,7 @@ const Home = () => {
                   to="/boards"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 md:py-4 md:text-lg md:px-10"
                 >
-                  Get started
+                  Sign up
                 </Link>
               </div>
               <div className="mt-3 sm:mt-0 sm:ml-3">
@@ -28,7 +28,7 @@ const Home = () => {
                   to="/boards/WKMKhsBtDVbl8SM1A6jW"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-teal-700 bg-gray-200 hover:bg-gray-300 md:py-4 md:text-lg md:px-10"
                 >
-                  See sample
+                  Try as Guest
                 </Link>
               </div>
             </div>
