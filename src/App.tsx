e1df6d59869bacc5fcd4ceffb8f48066659675ce
Chidi <chidex5@gmail.com>
2022-10-06 01:20:41 +0100

    create login and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import NotFound from './pages/404';
 import BoardDetails from './pages/boardDetails';
 import Dashboard from './pages/dashboard';
 import Home from './pages/home';
+import Login from './pages/login';
+import SignUp from './pages/signup';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App bg-teal-50">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
           <Route path="boards" element={<Dashboard />} />
           <Route path="boards">
             <Route path=":id" element={<BoardDetails />} />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import KanbanBoardImage from '../images/kanban-board.png';
+import KanbanBoardImage from '../assets/images/kanban-board.png';
 
 const Home = () => {
   return (
