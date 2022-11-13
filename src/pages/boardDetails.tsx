c921fea00a13dcbc49a145de7e2317b26fad9409
Chidi <chidex5@gmail.com>
2022-11-13 02:43:01 +0100

    Improve redirect on logout

--- src/pages/boardDetails.tsx
+++ src/pages/boardDetails.tsx
@@ -1,9 +1,20 @@
 import Layout from '../layout';
 import { useAuth } from '../firebase/auth/context';
 import BoardContainer from '../components/Board/BoardContainer';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const BoardDetails = () => {
-  const { authUser } = useAuth();
+  const { authUser, isLoading } = useAuth();
+
+  const navigate = useNavigate();
+
+  // Listen for changes to loading and authUser, redirect if needed
+  useEffect(() => {
+    if (!isLoading && !authUser) {
+      navigate('/');
+    }
+  }, [authUser, isLoading]);
 
   return <Layout>{authUser && <BoardContainer user={authUser} />}</Layout>;
 };
