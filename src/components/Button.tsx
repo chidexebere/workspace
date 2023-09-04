bd2e679548133e1365116e5a28dd6667afcc2bb9
Chidi <chidex5@gmail.com>
2023-09-04 06:29:20 +0100

    Modify button component to include types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
 interface Props {
   variant?: string;
-  handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   isDisabled?: boolean;
   children: React.ReactNode;
   value?: string;
+  type?: 'button' | 'reset' | 'submit';
 }
 
 const Button = ({
@@ -12,6 +13,7 @@ const Button = ({
   isDisabled,
   children,
   value,
+  type,
 }: Props) => {
   const defaultClass =
     'inline-block px-4 py-2 font-medium text-xs leading-normal rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out flex align-center';
@@ -20,9 +22,11 @@ const Button = ({
     ? `${defaultClass} ${variant}`
     : `${defaultClass}`;
 
+  const buttonType = type ? type : 'button';
+
   return (
     <button
-      type="button"
+      type={buttonType}
       disabled={isDisabled}
       className={buttonClass}
       value={value}
